f77c9c9c1b685c0a336d823fd4f4b130bd851581
Naym Hossen <naym100m@gmail.com>
2025-05-27 19:50:39 +0600

    unable fb login

--- src/utils/authOptions.ts
+++ src/utils/authOptions.ts
@@ -11,10 +11,10 @@ export const authOptions: NextAuthOptions = {
       clientId: process.env.GOOGLE_ID as string,
       clientSecret: process.env.GOOGLE_SECRET as string,
     }),
-    // FacebookProvider({
-    //   clientId: process.env.FACEBOOK_CLIENT_ID as string,
-    //   clientSecret: process.env.FACEBOOK_CLIENT_SECRET as string,
-    // }),
+    FacebookProvider({
+      clientId: process.env.FACEBOOK_CLIENT_ID as string,
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET as string,
+    }),
     // TwitterProvider({
     //   clientId: process.env.TWITTER_CLIENT_ID as string,
     //   clientSecret: process.env.TWITTER_CLIENT_SECRET as string,
