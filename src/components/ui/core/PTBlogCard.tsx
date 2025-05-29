453025327bf297ea6056316b384de18fea345bfe
Naym Hossen <naym100m@gmail.com>
2025-05-29 11:44:48 +0600

    updated

--- src/components/ui/core/PTBlogCard.tsx
+++ src/components/ui/core/PTBlogCard.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 import Link from "next/link";
 
 const PTBlogCard = ({ blog }: { blog: TBlogs }) => {
-  console.log("blog", blog);
+  // console.log("blog", blog);
   const dateString = new Date(blog.createdAt).toLocaleDateString(undefined, {
     year: "numeric",
     month: "long",
